ed65f39b534b48351abf212cd32ab01519f9c297
agtmarvel <mkmiller1359@proton.me>
2025-06-01 22:03:02 -0400

    yo static site done!

--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
 from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_directory_contents(src, dst):
+    srclist = os.listdir(src)    
+    
+    if os.path.exists(dst) == False:
+        os.mkdir(dst)
+    
+    for item in srclist:
+        if os.path.isfile(os.path.join(src,item)) == True:
+            src_path = shutil.copy2(os.path.join(src, item), os.path.join(dst, item))
+        elif os.path.isdir(os.path.join(src, item)) == True:
+            copy_directory_contents(os.path.join(src, item), os.path.join(dst, item))
+    pass
 
 def main():
-	node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-	print (node)
+    src = "static"
+    dst = "public"
+    if os.path.exists(dst) == True:
+        shutil.rmtree(dst)
+    
+    copy_directory_contents(src, dst)
+	
+
 
 if __name__ == "__main__":
 	main()
